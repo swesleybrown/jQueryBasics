bc2c30df73d53e7ee9191c235862616c651f156b
Wes Brown <swesleybrown@gmail.com>
2015-04-21 17:14:34 -0400

    edits

---
+++ js/scriptTraverse.js
@@ -0,0 +1,11 @@
+//$("li:first-child").css('background-color', 'steelblue');
+
+$("#unimportant-events").hide()
+
+//$( "#clickme" ).click(function() {
+//  $( "#unimportant-events" ).hide( "slow", function() {
+//    alert( "Animation complete." );
+//  });
+//});
+//$( "#important-events" ).children(console.log)
+console.log($( "#wrapper" ).children());
